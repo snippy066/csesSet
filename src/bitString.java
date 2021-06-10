2f97217c14d01b399a573c9676a01dd62569131d
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-10 18:38:51 +0530

    Update bitString.java

--- src/bitString.java
+++ src/bitString.java
@@ -7,7 +7,7 @@ public class bitString {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
        // BigInteger exp=BigInteger.valueOf(n);
-        BigInteger mod=new BigInteger("100000007");
+        BigInteger mod=new BigInteger("1000000007" );
         BigInteger val=new BigInteger("2");
         BigInteger ans=val.pow(n);
         System.out.println(ans.mod(mod));
