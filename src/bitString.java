45339da65cd514b028ebe2965c8c8ecfe2e7e7e2
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-10 18:32:34 +0530

    bit String CSES in java

--- src/bitString.java
+++ src/bitString.java
@@ -1,8 +1,14 @@
+
 import java.util.*;
+import java.math.*;
 
 public class bitString {
     public static void main(String ar[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        BigInteger exp=BigInteger.valueOf(n);
+        BigInteger mod=new BigInteger("100000007");
+        BigInteger val=new BigInteger("2");
+        System.out.println(val.modPow(exp,mod));
     }
 }
