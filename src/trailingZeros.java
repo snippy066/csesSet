8e4abdbd56ad5cd311d337e6705ed29f579d6151
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-10 18:49:13 +0530

    trailing zeros CSES in java

--- src/trailingZeros.java
+++ src/trailingZeros.java
@@ -1,3 +1,23 @@
+import java.util.*;
 
 public class trailingZeros {
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int twcount=0,ficount=0;
+
+        int cp=n;
+        while(cp>=0){
+            twcount++;
+           cp=cp>>1;
+        }
+        while(n>=0){
+            ficount++;
+            n/=5;
+        }
+
+        System.out.println(Math.min(twcount,ficount));
+    }
 }
