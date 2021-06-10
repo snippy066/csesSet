51bd09eb8c72dd0ffbc1aaaa8d6c316a93dd0620
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-10 18:59:19 +0530

    trailing zeroes

--- src/trailingZeros.java
+++ src/trailingZeros.java
@@ -5,18 +5,11 @@ public class trailingZeros {
         Scanner sc=new Scanner(System.in);
 
         int n=sc.nextInt();
-
         int twcount=0,ficount=0;
 
         int cp=n;
-        while(cp>0){
-            twcount++;
-           cp=cp>>1;
-        }
-        while(n>0){
-            ficount++;
-            n/=5;
-        }
+        twcount=cp/2;
+        ficount=n/5;
 
         System.out.println(Math.min(twcount,ficount));
     }
