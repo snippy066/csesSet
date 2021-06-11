42df1d6316f5a0ca41a202065067deed5de2b75a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-11 13:40:00 +0530

    Create palidromReorder.java

---
+++ src/palidromReorder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class palidromReorder {
+    private static HashMap<String,Integer> hm;
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        String st[]=sc.nextLine().split("");
+        getpalindrom(st,st.length());
+    }
+
+    static void getpalindrom(String[] s,int n){
+        hm=new HashMap<String,Integer>();
+
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+}
