6bce3058cdd48cb15687d19cf13ff3514a9640f3
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-11 14:20:09 +0530

    Update palidromReorder.java

--- src/palidromReorder.java
+++ src/palidromReorder.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.StringUtils;
+
 import java.util.*;
 
 public class palidromReorder {
@@ -19,6 +21,27 @@ public class palidromReorder {
             }
             else
                 hm.put(s[i],1);
+        }
+        int oddCount=0,flag=0;
+        String ch,firsth="",secondh="";
+        for(Map.Entry mp :hm.entrySet()){
+            if((int)mp.getValue()%2!=0){
+                oddCount++;
+                flag++;
+                ch=(String)mp.getKey();
+            }
+
+            if(flag>1)
+                break;
+        }
+        if(flag>1)
+            System.out.println("NO SOLUTION");
+        else if(flag>0 && n%2==0)
+            System.out.println("NO SOLUTION");
+        else{
+            for(Map.Entry mp :hm.entrySet()) {
+            String filled= StringUtils.repeat((String)mp.getKey(),(int)mp.getValue()/2);
+            }
         }
     }
 }
