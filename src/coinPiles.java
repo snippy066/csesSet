7a3afb5603d10af4d1a67df6baec2f5a97e9b93e
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-11 12:48:37 +0530

    Update coinPiles.java

--- src/coinPiles.java
+++ src/coinPiles.java
@@ -1,3 +1,22 @@
+import java.util.*;
 
 public class coinPiles {
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        StringBuilder sb=new StringBuilder();
+        while(t-->0){
+            int a=in.nextInt();
+            int b=in.nextInt();
+            
+            int sum=a+b;
+
+            if(sum%3==0)
+                sb.append("YES");
+            else
+                sb.append("NO");
+        }
+        System.out.println(sb.toString());
+    }
+
 }
