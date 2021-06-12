34ddcb99ca440c27b48f12137f39eeae370a6a4a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-12 13:25:21 +0530

    gray code

--- src/grayCode.java
+++ src/grayCode.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class grayCode {
     public static void main(String ar[]){
         Scanner sc=new Scanner(System.in);
-        
+        int n=sc.nextInt();
+        char[] ch=new char[n];
+        Arrays.fill(ch,'1');
+        String s=new String(ch);
+        int i=0;
+        while(++i<=4){
+        s=s.replaceFirst("1","0");
+        System.out.println(s);
+        }
     }
 }
