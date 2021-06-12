f78547ba6957c5ae68a62f630a8735e0b34dfec9
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-12 14:00:09 +0530

    gray code

--- src/grayCode.java
+++ src/grayCode.java
@@ -6,15 +6,21 @@ public class grayCode {
         int n=sc.nextInt();
         char[] ch=new char[n];
         Arrays.fill(ch,'0');
+        char add='1';
         String s=new String(ch);
         StringBuilder sb=new StringBuilder();
-        char add='1';
-        int su=0;
-        int i=s.length();
-        while(i>0||su==1){
-            su+=s.charAt(i);
-            
+        for(long i=0;i<Math.pow(2,n);i++) {
+            int su = 0;
+            String res = "";
+            int l = s.length()-1;
+            while (l >=0 || su == 1) {
+                su += s.charAt(l) + add;
+                res += (char) (su % 2+'0');
+                su /= 2;
+                l--;
+            }
+            sb.append(res + "\n");
         }
-
+        System.out.println(sb);
     }
 }
