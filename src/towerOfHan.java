aedcdbe1a3240060d90ea2b6c149b54c3895bb88
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-15 13:19:49 +0530

    Update towerOfHan.java

--- src/towerOfHan.java
+++ src/towerOfHan.java
@@ -7,9 +7,13 @@ public class towerOfHan {
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
         tower(n,"1","2","3");
-        System.out.println(sb);
+        System.out.println(Math.pow(2,n)-1+"\n"+sb);
     }
     static void tower(int n,String sr,String aux,String des){
-        
+        if(n==1)
+            sb.append(sr+" "+des+"\n");
+        tower(n-1,sr,aux,des);
+        sb.append(sr+" "+des+"\n");
+        tower(n-1,aux,des,sr);
     }
 }
