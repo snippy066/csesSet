78ac3324900940d5a7db9dfcec335fae2ff66015
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-15 13:16:39 +0530

    Update towerOfHan.java

--- src/towerOfHan.java
+++ src/towerOfHan.java
@@ -2,7 +2,14 @@ import java.util.*;
 
 
 public class towerOfHan {
+    private static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
         Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        tower(n,"1","2","3");
+        System.out.println(sb);
+    }
+    static void tower(int n,String sr,String aux,String des){
+        
     }
 }
