1ee23c9d5d916b508dbb17e67df7c5e0ca2a13e1
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-15 17:42:35 +0530

    Update createString.java

--- src/Introductory Problems/createString.java
+++ src/Introductory Problems/createString.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class createString {
-    private static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
-        Scanner sc=new Scanner(System.in);
-        String[] s=sc.nextLine().split("");
-        int n=s.length;
-        generatePs(s,n);
+        Scanner in=new Scanner(System.in);
+
+        String[] arr=in.nextLine().split("");
+        int n=arr.length;
+
+        permute(arr,0,n-1);
     }
 
-    static void generatePs(String[] arr,int len){
-        int  loop=(1<<len);
-
-        for(int i=0;i<loop;i++){
-            for(int j=0;j<len;j++) {
-                if ((i & (1 << j)) > 0)
-                    sb.append(arr[j]);
-            }
-            sb.append("\n");
-        }
-        System.out.print(sb.toString());
-
-    }
+    
 }
