32ea6322745a1f8a5023bc48cb4622cd0868c9b3
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-15 17:37:56 +0530

    create string cses introductory prob sol in java

--- src/Introductory Problems/createString.java
+++ src/Introductory Problems/createString.java
@@ -4,25 +4,20 @@ public class createString {
     private static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-
-        int[] arr=new int[n];
-
-        for(int i=0;i<n;i++)    arr[i]=sc.nextInt();
-
-        generatePs(arr,n);
+        String[] s=sc.nextLine().split("");
+        int n=s.length;
+        generatePs(s,n);
     }
 
-    static void generatePs(int[] arr,int len){
+    static void generatePs(String[] arr,int len){
         int  loop=(1<<len);
 
         for(int i=0;i<loop;i++){
-            sb.append("{");
             for(int j=0;j<len;j++) {
                 if ((i & (1 << j)) > 0)
-                    sb.append(arr[j] + " ");
+                    sb.append(arr[j]);
             }
-            sb.append("}\n");
+            sb.append("\n");
         }
         System.out.print(sb.toString());
 
