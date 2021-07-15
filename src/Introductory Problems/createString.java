87d2cb9ef4ec949efb275b67a127799ce47c44d1
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-15 17:48:46 +0530

    Update createString.java

--- src/Introductory Problems/createString.java
+++ src/Introductory Problems/createString.java
@@ -10,9 +10,24 @@ public class createString {
         int n=arr.length;
 
         permute(arr,0,n-1);
+        System.out.print(sb.toString());
     }
 
-    static void permute(String[] arr,int l,int r){
+    static void permute(String[] a,int l,int r){
+        if(l==r)
+            sb.append(arr);
+        else{
 
+            for(int i=l;i<=r;i++){
+                swap(a[l],a[i);
+                permute(a,l+1,r);
+                swap(a[l],a[i]);
+            }
+        }
+    }
+    static void swap(String x,String y){
+        String temp=x;
+        x=y;
+        y=temp;
     }
 }
