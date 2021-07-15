18d018fb4ed49a82a6c719871dc63f51a62dafcf
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-15 17:49:07 +0530

    Update createString.java

--- src/Introductory Problems/createString.java
+++ src/Introductory Problems/createString.java
@@ -19,7 +19,7 @@ public class createString {
         else{
 
             for(int i=l;i<=r;i++){
-                swap(a[l],a[i);
+                swap(a[l],a[i]);
                 permute(a,l+1,r);
                 swap(a[l],a[i]);
             }
