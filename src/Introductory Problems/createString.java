5c3a677af96a70bce1f46a6559365871501197d7
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-15 18:23:18 +0530

    Update createString.java

--- src/Introductory Problems/createString.java
+++ src/Introductory Problems/createString.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 public class createString {
     private static StringBuilder sb=new StringBuilder();
-    private static String a;
     public static void main(String ar[]){
         Scanner in=new Scanner(System.in);
-        a=in.nextLine();
-        int n=a.length();
+        String[] arr=in.nextLine().split("");
+        int n=arr.length;
 
-        permute(0,n-1);
+        permute(arr,0,n-1);
         System.out.print(sb.toString());
     }
 
-    static void permute(int l,int r){
+    static void permute(String[] a,int l,int r){
         if(l==r)
-            sb.append(a);
+            sb.append(a[l]);
         else{
 
             for(int i=l;i<=r;i++){
                 swap(a[l],a[i]);
-                permute(l+1,r);
+                permute(a,l+1,r);
                 swap(a[l],a[i]);
             }
         }
