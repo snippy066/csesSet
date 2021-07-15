18d4c33930b6b306513e9eff25087eecff51be8c
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-15 19:06:02 +0530

    creating String cses prob sol in java

--- src/Introductory Problems/createString.java
+++ src/Introductory Problems/createString.java
@@ -2,30 +2,42 @@ import java.util.*;
 
 public class createString {
     private static StringBuilder sb=new StringBuilder();
+    private static HashSet<String> hs=new HashSet<>();
     public static void main(String ar[]){
         Scanner in=new Scanner(System.in);
-        String[] arr=in.nextLine().split("");
-        int n=arr.length;
+        String arr=in.nextLine();
+        int n=arr.length();
 
         permute(arr,0,n-1);
-        System.out.print(sb.toString());
+
+        List<String> li=new ArrayList<>(hs);
+        Collections.sort(li);
+
+        System.out.println(li.size());
+        for(String st:li)
+            sb.append(st+"\n");
+
+        System.out.println(sb.toString());
     }
 
-    static void permute(String[] a,int l,int r){
+    static void permute(String a,int l,int r){
         if(l==r)
-            sb.append(a[l]);
+            hs.add(a);
         else{
 
             for(int i=l;i<=r;i++){
-                swap(a[l],a[i]);
+                char s1[]=a.toCharArray();
+                a=swap(s1,l,i);
                 permute(a,l+1,r);
-                swap(a[l],a[i]);
+                a=swap(s1,l,i);
             }
         }
     }
-    static void swap(String x,String y){
-        String temp=x;
-        x=y;
-        y=temp;
+    static String swap(char[] a,int l, int r ){
+       char tem=a[l];
+       a[l]=a[r];
+       a[r]=tem;
+
+       return String.valueOf(a);
     }
 }
