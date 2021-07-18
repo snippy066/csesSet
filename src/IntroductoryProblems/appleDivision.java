4a99e86cec0ba24888da465fe8143fcb550a7e23
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-18 15:10:23 +0530

    Update appleDivision.java

--- src/IntroductoryProblems/appleDivision.java
+++ src/IntroductoryProblems/appleDivision.java
@@ -15,5 +15,17 @@ public class appleDivision {
         getdiff(arr,n);
     }
 
-    
+    static void getdiff(int[] arr,int n){
+        int sum1=0,sum2=0,min=Integer.MAX_VALUE;
+        for(int i=0;i<(int)Math.pow(2,n);i++){
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))>0) {
+                    sum1 += arr[j];
+                }
+                else
+                    sum2+=arr[j];
+            }
+            
+        }
+    }
 }
