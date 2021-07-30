dd338c32f86dd41df16cc527c59856a775183321
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-30 16:49:15 +0530

    Update distinctNum.java

--- src/sortingAndSearching/distinctNum.java
+++ src/sortingAndSearching/distinctNum.java
@@ -3,7 +3,7 @@
 //import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
+//import java.util.Set;
 
 public class distinctNum {
     private static StringBuilder sb=new StringBuilder();
@@ -12,12 +12,14 @@ public class distinctNum {
 
         int n=in.nextInt();
 
-        Set<Integer> hs = new HashSet<Integer>();
+        
 
-        for(int i=0;i<n;i++){
-            int key=in.nextInt();
-            hs.add(key);
-        }
+        HashSet<Integer> hs = new HashSet<Integer>();
+
+//        for(int i=0;i<n;i++){
+//
+//            hs.add(in.nextInt());
+//        }
         System.out.println(hs.size());
     }
 }
