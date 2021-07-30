887b597ad4a25cc7edbb19607ff394a7e1ace80a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-30 17:17:02 +0530

    Update aparments.java

--- src/sortingAndSearching/aparments.java
+++ src/sortingAndSearching/aparments.java
@@ -1,4 +1,44 @@
 package sortingAndSearching;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class aparments {
+    private static StringBuilder sb=new StringBuilder();
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+
+        int n=in.nextInt();
+        int m=in.nextInt();
+        int k=in.nextInt();
+
+        int[] desi=new int[n];
+        int[] rel=new int[m];
+
+        for(int i=0;i<n;i++)    desi[i]=in.nextInt();
+        for(int i=0;i<m;i++)    rel[i]=in.nextInt();
+
+        Arrays.sort(desi);
+        Arrays.sort(rel);
+
+        int i=0,j=0,count=0;
+        while(i<n && j<m){
+            int diff=desi[i]-rel[i];
+
+            if(Math.abs(diff)<=k){
+                i++; j++; count++;
+            }
+            else{
+                if(diff>k)
+                    j++;
+
+                else
+                    i++;
+
+            }
+
+        }
+
+        System.out.println(count);
+    }
 }
