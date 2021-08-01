3bd093419a961035f1603fa3b22a11cfc6e8841f
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-01 23:53:45 +0530

    Update ferrisWheel.java

--- src/sortingAndSearching/ferrisWheel.java
+++ src/sortingAndSearching/ferrisWheel.java
@@ -1,4 +1,25 @@
 package sortingAndSearching;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ferrisWheel {
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+
+        int n=in.nextInt();
+        int x=in.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)    arr[i]=in.nextInt();
+
+        Arrays.sort(arr);
+
+        int count=0;
+
+        while(true){
+            
+        }
+    }
 }
